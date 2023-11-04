9084111b6f0bad2b0a9085efa9d176d231413d83
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2023-11-04 18:34:11 +0300

    feat: Chat Application updated
    WebSocketEventListener.java class created

---
+++ chat/src/main/java/com/serdarfirlayis/chat/config/WebSocketEventListener.java
@@ -0,0 +1,18 @@
+package com.serdarfirlayis.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ){
+        // TODO: to be implemented
+    }
+}
