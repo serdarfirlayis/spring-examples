c29df463e571b16f84cd12f7c52f9f2c17edf3dd
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2023-11-06 02:47:23 +0300

    feat: Chat Application updated
    ChatController.java, ChatMessage.java, MessageType.java classes created and WebSocketEventListener.java class updated

---
+++ chat/src/main/java/com/serdarfirlayis/chat/chat/ChatController.java
@@ -0,0 +1,31 @@
+package com.serdarfirlayis.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    // explanation: if a message is sent to /chat.sendMessage, the message will be sent to /topic/public
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage
+    ) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor
+    ){
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
---
+++ chat/src/main/java/com/serdarfirlayis/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.serdarfirlayis.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
