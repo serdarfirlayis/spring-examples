c106a07751f945953041f4390fda3fa321a8220f
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2023-11-15 22:11:35 +0300

    feat: Chat Application updated
    index.html, main.js, main.css classes updated

---
+++ chat/src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var username: Element = document.querySelector('#username-page');
+var chatPage: Element = document.querySelector('#chat-page');
+
+var usernameForm = document.querySelector('#usernameForm');
+var messageForm = document.querySelector('#messageForm');
+var messageInput = document.querySelector('#message');
+var messageArea = document.querySelector('#messageArea');
+var connectingElement = document.querySelector('.connecting');
