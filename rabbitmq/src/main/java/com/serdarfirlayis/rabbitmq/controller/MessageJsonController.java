5c174ff63d42983adaafa5533396ef8f776a463e
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-11 21:11:06 +0300

    :sparkles: feat: Added MessageJsonController.java class to create REST API to Send JSON Object to RabbitMQ Producer

---
+++ rabbitmq/src/main/java/com/serdarfirlayis/rabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,23 @@
+package com.serdarfirlayis.rabbitmq.controller;
+
+import com.serdarfirlayis.rabbitmq.dto.User;
+import com.serdarfirlayis.rabbitmq.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private final RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json Message sent to the RabbitMQ ...");
+    }
+}
