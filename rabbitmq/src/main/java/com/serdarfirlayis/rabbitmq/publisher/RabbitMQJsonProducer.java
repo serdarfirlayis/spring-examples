eca5824aeb9d829e72076a0b96867163b8748424
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-11 20:09:36 +0300

    :sparkles: feat: Added RabbitMQJsonProducer.java class to produce and send message in Json type

---
+++ rabbitmq/src/main/java/com/serdarfirlayis/rabbitmq/publisher/RabbitMQJsonProducer.java
@@ -0,0 +1,31 @@
+package com.serdarfirlayis.rabbitmq.publisher;
+
+import com.serdarfirlayis.rabbitmq.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+   @Value("${rabbitmq.routing.json.key}")
+    private String routingKey;
+
+   private final RabbitTemplate rabbitTemplate;
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+
+    public RabbitMQJsonProducer(RabbitTemplate rabbitTemplate) {
+         this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user) {
+        LOGGER.info(String.format("Json Message sent -> %s", user.toString()));
+        rabbitTemplate.convertAndSend(exchange, routingKey, user);
+    }
+}
